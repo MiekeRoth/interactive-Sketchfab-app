d8b66aefb703276753c925bd176553c48507bffc
MiekeRoth <77438786+MiekeRoth@users.noreply.github.com>
2022-02-03 12:54:08 +0100

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,788 @@
+const visibilitygroups = {
+  tutorial: {
+    title: 'tutorial',
+    options: [
+      {
+        name: 'hide',
+        objectnames: [
+        ]
+      },
+      {
+        name: 'all',
+        objectnames: [
+ 'mucous_gland1_2',
+ 'mucous_gland2_3',
+ 'mucous_gland3_4',
+ 'mucous_gland_5',
+ 'slakovotestist_6',
+ 'dartsack_7',
+ 'lung_aderen2_8',
+ 'salivary_glands_9',
+ 'kidney_10',
+ 'lung_11',
+ 'digestive_gland_12',
+ 'hart_aderen1_13',
+ 'penis_14',
+ 'digestive_tract1_15',
+ 'skin_muscle_mouth_nerves_16',
+ 'footmuscle_17',
+ 'printbaar_huisje_18',
+ 'intern_lichaam_19',
+ 'digestive tract2_20',
+ 'reproductive tract_21',
+ 'ovoduct_22',
+ 'ovoduct1_23',
+ 'zenuwstelsel_24', 
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'lung_aderen2.001_31',
+ 'salivary_glands.001_32',
+ 'lung.001_33',
+ 'digestive_gland.001_34',
+ 'hart_aderen1.001_35',
+ 'penis.001_36',
+ 'digestive_tract1.001_37',
+ 'skin_muscle_mouth_nerves.001_38',
+ 'footmuscle.001_39',
+ 'printbaar_huisje.001_40',
+ 'intern_lichaam.002_41',
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+ 'kidney.001_45',
+ 'digestive tract2.001_46',
+ 'zenuwstelsel.001_47',
+  ]
+      },
+      {
+        name: 'a0',
+        objectnames: [
+			'printbaar_huisje_18',
+			'intern_lichaam_19',    ]
+      },
+      {
+        name: 'a1',
+        objectnames: [
+			'printbaar_huisje_18',
+			'intern_lichaam_19', 
+        ]
+      },
+      {
+        name: 'a2',
+        objectnames: [
+			'footmuscle_17',
+			'printbaar_huisje_18',
+			'intern_lichaam.002_41', 
+        ]
+      },
+		{
+        name: 'a3',
+        objectnames: [
+ 	'intern_lichaam_19',    ]
+      },
+      {
+        name: 'a4',
+        objectnames: [
+ 'mucous_gland1_2',
+ 'mucous_gland2_3',
+ 'mucous_gland3_4',
+ 'mucous_gland_5',
+ 'slakovotestist_6',
+ 'dartsack_7',
+ 'lung_aderen2_8',
+ 'salivary_glands_9',
+ 'kidney_10',
+ 'lung_11',
+ 'digestive_gland_12',
+ 'hart_aderen1_13',
+ 'penis_14',
+ 'digestive_tract1_15',
+ 'skin_muscle_mouth_nerves_16',
+ 'digestive tract2_20',
+ 'reproductive tract_21',
+ 'ovoduct_22',
+ 'ovoduct1_23',
+ 'zenuwstelsel_24',
+'intern_lichaam.002_41',
+    ]
+      },
+      {
+        name: 'a5',
+        objectnames: [
+
+ 'salivary_glands_9',
+ 'digestive_gland_12',
+  'digestive_tract1_15',
+  'digestive tract2_20',
+ 'intern_lichaam.002_41',
+
+        ]
+      },
+      {
+        name: 'a6',
+        objectnames: [
+ 'digestive tract2_20',
+ 'salivary_glands.001_32',
+ 'digestive_gland.001_34',
+ 'digestive_tract1.001_37',
+ 
+ ]
+      },
+      {
+        name: 'a7',
+        objectnames: [
+ 'digestive tract2_20',
+ 'salivary_glands.001_32',
+ 'digestive_gland.001_34',
+ 'digestive_tract1.001_37',
+ 	]
+      },
+		{
+        name: 'a8',
+        objectnames: [
+'digestive tract2_20',
+ 'salivary_glands_9',
+ 'digestive_gland.001_34',
+ 'digestive_tract1.001_37',
+]
+      },
+      {
+        name: 'a9',
+        objectnames: [
+ 
+'digestive tract2_20',
+ 'salivary_glands.001_32',
+ 'digestive_gland.001_34',
+ 'digestive_tract1.001_37',
+ ]
+      },
+      {
+        name: 'a10',
+        objectnames: [
+ 'salivary_glands.001_32',
+ 'digestive_gland_12',
+ 'digestive_tract1.001_37',
+ 'digestive tract2.001_46',   ]
+      },
+      {
+        name: 'a11',
+        objectnames: [
+'digestive tract2_20',
+ 'salivary_glands.001_32',
+ 'digestive_gland.001_34',
+ 'digestive_tract1.001_37',
+
+  ]
+      },
+      {
+        name: 'a12',
+        objectnames: [
+ 'lung_aderen2_8',
+ 'kidney_10',
+ 'lung_11',
+ 'hart_aderen1_13',
+ 'intern_lichaam.002_41',
+		]
+      },
+      {
+        name: 'a13',
+        objectnames: [
+'hart_aderen1_13',
+ 'lung_aderen2.001_32',
+ 'kidney.001_45',
+ 'lung.001_33',
+   ]
+      },
+      {
+        name: 'a14',
+        objectnames: [
+'lung_11',
+'lung_aderen2.001_32',
+  'kidney.001_45',
+ 'hart_aderen1.001_35',
+    ]
+      },
+      {
+        name: 'a15',
+        objectnames: [
+
+ 'lung_aderen2_8',
+ 'hart_aderen1_13',
+ 'kidney.001_45',
+ 'lung.001_33',
+			
+        ]
+      },
+      {
+        name: 'a16',
+        objectnames: [
+
+ 'kidney_10',
+ 'lung_aderen2.001_31',
+ 'lung.001_33',
+ 'hart_aderen1.001_35',
+
+   ]
+      },
+      {
+        name: 'a17',
+        objectnames: [
+ 'mucous_gland1_2',
+ 'mucous_gland2_3',
+ 'mucous_gland3_4',
+ 'mucous_gland_5',
+ 'slakovotestist_6',
+ 'dartsack_7',
+ 'penis_14',
+ 'reproductive tract_21',
+ 'ovoduct_22',
+ 'ovoduct1_23',
+ 'intern_lichaam.002_41',
+  ]
+      },
+      {
+        name: 'a18',
+        objectnames: [
+ 'reproductive tract_21',
+'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+      ]
+      },
+      {
+        name: 'a19',
+        objectnames: [
+'reproductive tract_21',
+'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+  'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44',     ]
+      },
+		{
+        name: 'a20',
+        objectnames: [
+ 'penis_14',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+  ]
+      },
+		{
+        name: 'a21',
+        objectnames: [  
+		 'dartsack_7',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+  'penis.001_36',
+
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+        ]
+      },
+  {
+        name: 'a22',
+        objectnames: [   
+ 'mucous_gland1_2',
+ 'mucous_gland2_3',
+ 'mucous_gland3_4',
+ 'mucous_gland_5',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+       ]
+      },
+{
+        name: 'a23',
+        objectnames: [   		
+  'penis_14',
+  'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+   ]
+      },	
+{
+        name: 'a24',
+        objectnames: [   		
+  'slakovotestist_6',
+  'ovoduct_22',
+  'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+  'dartsack.001_30',
+ 'penis.001_36',
+
+ 'reproductive tract.001_42',
+  'ovoduct1.001_44', 
+   ]
+      },	
+{
+        name: 'a25',
+        objectnames: [   		
+ 'slakovotestist_6',
+ 'ovoduct_22',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+ 'reproductive tract.001_42',
+ 'ovoduct1.001_44', 
+    ]
+      },	
+{
+        name: 'a26',
+        objectnames: [   		
+ 'reproductive tract_21',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 
+   ]
+      },	
+{
+        name: 'a27',
+        objectnames: [   		
+  'reproductive tract_21',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+ 'ovoduct.001_43',
+ 'ovoduct1.001_44', 	
+   ]
+      },	
+{
+        name: 'a28',
+        objectnames: [   		
+ 'reproductive tract_21',
+ 'ovoduct1_23',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+ 'reproductive tract.001_42',
+ 'ovoduct.001_43',
+   ]
+      },	
+{
+        name: 'a29',
+        objectnames: [   		
+ 'reproductive tract_21',
+ 'ovoduct_22',
+ 'ovoduct1_23',
+ 'mucous_gland1.001_25',
+ 'mucous_gland2.001_26',
+ 'mucous_gland3.001_27',
+ 'mucous_gland.001_28',
+ 'slakovotestist.001_29',
+ 'dartsack.001_30',
+ 'penis.001_36',
+
+
+   ]
+      },	
+{
+        name: 'a30',
+        objectnames: [   		
+ 'skin_muscle_mouth_nerves_16',
+ 'zenuwstelsel_24', 
+'digestive tract2.001_46',
+ 'intern_lichaam.002_41',
+    ]
+      },	
+{
+        name: 'a31',
+        objectnames: [  
+ 'zenuwstelsel_24', 
+ 'skin_muscle_mouth_nerves.001_38',
+ 'digestive tract2.001_46',
+   ]
+      },	
+{
+        name: 'a32',
+        objectnames: [   		
+ 'zenuwstelsel_24', 
+ 'skin_muscle_mouth_nerves.001_38',
+
+   'digestive tract2.001_46', ]
+      },	
+{
+        name: 'a33',
+        objectnames: [   		
+ 'zenuwstelsel_24', 
+ 'skin_muscle_mouth_nerves.001_38',
+
+ 'digestive tract2.001_46',
+   ]
+      },	
+{
+        name: 'a34',
+        objectnames: [   		
+ 'zenuwstelsel_24', 
+ 'skin_muscle_mouth_nerves.001_38',
+ 'digestive tract2.001_46',		
+   ]
+      },	
+{
+        name: 'a35',
+        objectnames: [   		
+ 'mucous_gland1_2',
+ 'mucous_gland2_3',
+ 'mucous_gland3_4',
+ 'mucous_gland_5',
+ 'slakovotestist_6',
+ 'dartsack_7',
+ 'lung_aderen2_8',
+ 'salivary_glands_9',
+ 'kidney_10',
+ 'lung_11',
+ 'digestive_gland_12',
+ 'hart_aderen1_13',
+ 'penis_14',
+ 'digestive_tract1_15',
+ 'skin_muscle_mouth_nerves_16',
+ 'footmuscle_17',
+ 'digestive tract2_20',
+ 'reproductive tract_21',
+ 'ovoduct_22',
+ 'ovoduct1_23',
+ 'zenuwstelsel_24', 
+  'intern_lichaam.002_41',
+    ]
+      },	
+{
+        name: 'a36',
+        objectnames: [   		
+  'printbaar_huisje_18',
+ 'intern_lichaam_19',
+    ]
+      },		
+    ]
+  }
+} 
+
+const eventData = [
+  {
+    name: 'a0',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a0',
+    annotation: 0,
+    animation: null
+  },
+  {
+    name: 'a1',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a1',
+    annotation: 1,
+    animation: null
+  },
+  {
+    name: 'a2',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a2',
+    annotation: 2,
+    animation: null
+  },
+  {
+    name: 'a3',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a3',
+    annotation: 3,
+    animation: null
+  },
+  {
+    name: 'a4',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a4',
+    annotation: 4,
+    animation: null
+  },
+  {
+    name: 'a5',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a5',
+    annotation: 5,
+    animation: null
+  },
+  {
+    name: 'a6',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a6',
+    annotation: 6,
+    animation: null
+  },
+  {
+    name: 'a7',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a7',
+    annotation: 7,
+    animation: null
+  },
+  {
+    name: 'a8',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a8',
+    annotation: 8,
+    animation: null
+  },
+  {
+    name: 'a9',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a9',
+    annotation: 9,
+    animation: null
+  },
+  {
+    name: 'a10',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a10',
+    annotation: 10,
+    animation: null
+  },
+  {
+    name: 'a11',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a11',
+    annotation: 11,
+    animation: null
+  },
+  {
+    name: 'a12',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a12',
+    annotation: 12,
+    animation: null
+  },
+  {
+    name: 'a13',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a13',
+    annotation: 13,
+    animation: null
+  },
+	  {
+    name: 'a14',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a14',
+    annotation: 14,
+    animation: null
+  },
+	  {
+    name: 'a15',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a15',
+    annotation: 15,
+    animation: null
+  },
+	  {
+    name: 'a16',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a16',
+    annotation: 16,
+    animation: null
+  },
+  {
+    name: 'a17',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a17',
+    annotation: 17,
+    animation: null
+  },
+	  {
+    name: 'a18',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a18',
+    annotation: 18,
+    animation: null
+  },
+	  {
+    name: 'a19',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a19',
+    annotation: 19,
+    animation: null
+  },
+		  {
+    name: 'a20',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a20',
+    annotation: 20,
+    animation: null
+  },
+		  {
+    name: 'a21',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a21',
+    annotation: 21,
+    animation: null
+  },
+		  {
+    name: 'a22',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a22',
+    annotation: 22,
+    animation: null
+  },
+		  {
+    name: 'a23',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a23',
+    annotation: 23,
+    animation: null
+  },
+		  {
+    name: 'a24',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a24',
+    annotation: 24,
+    animation: null
+  },
+		  {
+    name: 'a25',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a25',
+    annotation: 25,
+    animation: null
+  },
+		  {
+    name: 'a26',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a26',
+    annotation: 26,
+    animation: null
+  },
+		  {
+    name: 'a27',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a27',
+    annotation: 27,
+    animation: null
+  },
+		  {
+    name: 'a28',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a28',
+    annotation: 28,
+    animation: null
+  },
+		  {
+    name: 'a29',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a29',
+    annotation: 29,
+    animation: null
+  },
+		
+ 		  {
+    name: 'a30',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a30',
+    annotation: 30,
+    animation: null
+  },
+		  {
+    name: 'a31',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a31',
+    annotation: 31,
+    animation: null
+  },
+		  {
+    name: 'a32',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a32',
+    annotation: 32,
+    animation: null
+  },
+		  {
+    name: 'a33',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a33',
+    annotation: 33,
+    animation: null
+  },
+		  {
+    name: 'a34',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a34',
+    annotation: 34,
+    animation: null
+  },
+		  {
+    name: 'a35',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a35',
+    annotation: 35,
+    animation: null
+  },
+		  {
+    name: 'a36',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a36',
+    annotation: 36,
+    animation: null
+  },
+		  {
+    name: 'a37',
+    visibilitygroup: 'tutorial',
+    visibilityoption: 'a37',
+    annotation: 37,
+    animation: null
+  },
+]
